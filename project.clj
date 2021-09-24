6bff9f44db41eca221aede18ced3e59ffa61effb
Smart-Y <s.artem@gmail.com>
2021-09-23 21:27:49 -0500

    Update project.clj

--- project.clj
+++ project.clj
@@ -32,6 +32,7 @@
                                        :output-to       "resources/public/js/compiled/app.js"
                                        :optimizations   :advanced}}]}
   :plugins [[lein-cljsbuild "1.1.5"]]
+  :min-lein-version "1.1.5"
   :test-paths ["test/clj"]
   :profiles {:uberjar {:aot        :all
                        :jvm-opts ["-Xms1024m" "-Xmx2048m"]
@@ -103,5 +104,4 @@
                  [kibu/pushy "0.3.8"]
                  [com.cognitect/transit-clj "0.8.300"]
                  [io.clojure/liberator-transit "0.3.1"]
-                 [liberator "0.15.1"]]
-:min-lein-version "1.1.5")
+                 [liberator "0.15.1"]])
